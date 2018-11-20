7bab5a9158a79e18d3279f11ad1651fa693ccdab
Felix Kleinert <f.kleinert@fz-juelich.de>
2018-11-20 14:17:35 +0100

    add new add_function

---
+++ printNumbers/functions/add_int_or_float.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#
+# add_int_or_float.py
+#
+# This file is part of printNumbers.
+#
+# Copyright (C) 2018 F. Kleinert, FSD, JSC, FZ Jülich
+#
+# printNumbers is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# printNumbers is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with printNumbers.  If not, see <http://www.gnu.org/licenses/>.
+
+def add_two_values(a, b):
+    '''
+    This function adds two values a and b
+    :param a: as int or float
+    :param b: as int or float
+    :return: sum as int or float depending in input type
+    '''
+    sum = a + b
+    return sum
